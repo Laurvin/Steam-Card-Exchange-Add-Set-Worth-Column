436ba2893e3ca56424593925fc7c1aa20abd3350
Laurvin <github@dbbv.com>
2017-06-10 04:02:16 +0200

    Create Steam_Card_Exchange_Add_Set_Worth_Column.user.js

--- Steam_Card_Exchange_Add_Set_Worth_Column.user.js
+++ Steam_Card_Exchange_Add_Set_Worth_Column.user.js
@@ -3,7 +3,7 @@
 // @namespace Steam Card Exchange Add Set Worth Column
 // @author Laurvin
 // @description Adds Set Worth
-// @version 1.2
+// @version 2.0
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Steam-Card-Exchange-Add-Set-Worth-Column/raw/master/Steam_Card_Exchange_Add_Set_Worth_Column.user.js
 // @include http://www.steamcardexchange.net/index.php?userlist
@@ -14,41 +14,51 @@
 // @run-at document-idle
 // ==/UserScript==
 
+// http://www.steamcardexchange.net/api/request.php?GetWatchlist_Private
+// http://www.steamcardexchange.net/api/request.php?GetInventory
+
 $( document ).ready(function()
-	{
+{
 	console.log("Starting Steam Card Exchange Add Set Worth Column!");
-	$("#inventorylist tr:first").append('<th title="Set Worth">S W</th>');
 
-	var MyRows = $('#inventorylist').find('tbody').find('tr');
+	var TableID = (window.location.href.indexOf("userlist") > -1)? 'private_watchlist' : 'inventorylist';
+	var InitialSort = (window.location.href.indexOf("userlist") > -1)? 'desc' : 'asc';
 
-	for (var i = 0; i < MyRows.length; i++)
+	// Setting paging to All or everything will bork.
+	$(`#${TableID}`).on( 'init.dt', function ()
 	{
-		var Worth = $(MyRows[i]).find('td:eq(1)').text();
-		Worth = Worth.substring(0, Worth.length - 1);
-		var SetSize = $(MyRows[i]).find('td:eq(3)').text();
-		SetSize = SetSize.substring(SetSize.length - 9, SetSize.length - 7);
-		var SetWorth = Worth*SetSize;
-		$(MyRows[i]).append('<td>'+SetWorth+'</td>');
-	}
-
-	if (window.location.href == "http://www.steamcardexchange.net/index.php?userlist")
-	{
-		var initOptions =
+		if ($(`#${TableID}`).DataTable().page.len() != -1)
 		{
-			widgets: ["zebra"],
-			sortList: [[4,1]]
-		};
-	}
-	else
+			$(`#${TableID}`).DataTable().page.len(-1).draw();
+		}
+	} ).dataTable();
+	
+	function ChangeTable(TableID, InitialSort)
 	{
-		var initOptions =
-		{
-			widgets: ["zebra"],
-			sortList: [[4,0]]
-		};		
-	}
+		$(`#${TableID} tr:first`).append('<th title="Set Worth">S W</th>');
 
-	$("#inventorylist").trigger("destroy");
-	$("#inventorylist").tablesorter(initOptions);
-	console.log("Finished Steam Card Exchange Add Set Worth Column!");
+		var MyRows = $(`#${TableID}`).find('tbody').find('tr');
+		console.log("Rows", MyRows.length);
+		for (var i = 0; i < MyRows.length; i++)
+		{
+			var Worth = $(MyRows[i]).find('td:eq(1)').text();
+			var SetSize = $(MyRows[i]).find('td:eq(3)').text();
+			SetSize = SetSize.substring(SetSize.length - 9, SetSize.length - 7);
+			var SetWorth = Worth*SetSize;
+			$(MyRows[i]).append('<td>'+SetWorth+'</td>');
+		}
+
+		$(`#${TableID}`).dataTable( {
+			"searching": false,
+			"destroy": true,
+			pageLength: -1,
+			autoWidth: false,
+			stateSave: true,
+			"order": [[ 4, `${InitialSort}` ], [ 0, 'asc' ]]
+		} );
+
+		console.log("Finished Steam Card Exchange Add Set Worth Column!");
+	}
+	
+	myVar = setTimeout(ChangeTable, 2500, TableID, InitialSort);
 });
